c03fbbf916cbf94078456606da517190813f6c6b
samsul-rijal <samsulr99@gmail.com>
2022-03-02 14:18:31 +0700

    update

--- src/screens/event.js
+++ src/screens/event.js
@@ -1,14 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, TouchableOpacity  } from "react-native";
 
 export default function Event() {
   // Create Function Here
+  function Greeting() {
+    return alert("Good Morning guys")
+  }
 
   return (
     <View>
       <StatusBar />
-      {/* Code Here */}
+      
+      <TouchableOpacity onPress={Greeting}>
+        Greeting
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => alert("Hello Batch 30")}>
+        Click here
+      </TouchableOpacity>
+
+
     </View>
   );
 }
