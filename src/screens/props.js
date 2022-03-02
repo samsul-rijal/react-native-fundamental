a7866eb18d4c63bafa167b31101e744abfdf14fa
samsul-rijal <samsulr99@gmail.com>
2022-03-02 14:22:17 +0700

    update

--- src/screens/props.js
+++ src/screens/props.js
@@ -1,14 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View } from "react-native";
+import List from "../components/list";
 
 export default function Props() {
   // Create variable to insert link Image
-
+  
   return (
     <View>
       <StatusBar />
-      {/* Code Here */}
+      <List listData="Avanza" />
+      <List listData="Ayla" />
+      <List listData="Brio" />
     </View>
   );
 }
