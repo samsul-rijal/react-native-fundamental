f445246f83d65968fd9cd92dd36bec15572bfe99
samsul-rijal <samsulr99@gmail.com>
2022-03-02 14:11:05 +0700

    update

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import React from "react";
 import { View } from "react-native";
 
 //Import Screen
+import EmbedExpression from "./src/screens/embedExpression";
 
 export default function App() {
   return (
     <View>
       <StatusBar />
-      {/* Use Component */}
+      <EmbedExpression />
     </View>
   );
 }
--- src/screens/embedExpression.js
+++ src/screens/embedExpression.js
@@ -1,16 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 export default function EmbedExpression() {
   // Create Function Here
+  function getBatch() {
+    return "Batch 30"
+  }
 
   // Create a variable Here
+  const companyName = "Dumbways.id"
 
   return (
     <View>
       <StatusBar />
-      {/* Code Here */}
+      <Text>
+        Welcome to {companyName} class {getBatch()}
+      </Text>
     </View>
   );
 }
