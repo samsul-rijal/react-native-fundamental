9239a1a81a879e00356cf1e51787c0b876b65cc4
samsul-rijal <samsulr99@gmail.com>
2022-03-02 14:53:17 +0700

    update

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import React from "react";
 import { View } from "react-native";
 
 //Import Screen
+import Form from "./src/screens/form";
 
 export default function App() {
   return (
     <View>
       <StatusBar />
-      {/* Use Component */}
+      <Form />
     </View>
   );
 }
